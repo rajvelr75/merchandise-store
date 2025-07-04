677e3def789cafcd496ddd8177c4aaa6a8be4a4f
Rajvel <rajvelr755@gmail.com>
2025-07-05 02:36:16 +0530

    Footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,72 +1,49 @@
 export const Footer = () => {
   return (
-    <footer className="bg-[#193f64] text-white py-12 px-4 md:px-10 font-[Source_Sans_Pro]">
-      <div className="max-w-7xl mx-auto flex flex-col lg:flex-row justify-between gap-12">
+    <footer className="bg-[#193f64] text-white py-10 px-4 font-[Source_Sans_Pro]">
+      <div className="max-w-7xl mx-auto flex flex-col lg:flex-row justify-between gap-y-10 gap-x-16">
+        
         {/* Logo Section */}
-        <div className="flex-shrink-0 flex justify-center lg:justify-start lg:ml-20">
+        <div className="flex justify-center lg:justify-start w-full lg:w-auto">
           <img
             src="https://c.animaapp.com/uSmv2gID/img/srmist-logo-hwite-3--1@2x.png"
             alt="SRMIST Logo"
-            className="w-[180px] md:w-[220px] object-contain"
+            className="w-[160px] md:w-[200px] object-contain"
           />
         </div>
 
         {/* Contact Section */}
-        <div className="flex flex-col gap-4 items-start">
+        <div className="flex flex-col gap-4 items-center lg:items-start w-full lg:w-auto text-center lg:text-left">
           {[
-            {
-              icon: "https://c.animaapp.com/uSmv2gID/img/icon.svg",
-              text: "+91 44 27417000",
-            },
-            {
-              icon: "https://c.animaapp.com/uSmv2gID/img/icon-1.svg",
-              text: "+91 44 27417777",
-            },
-            {
-              icon: "https://c.animaapp.com/uSmv2gID/img/icon-2.svg",
-              text: "+080 69087000",
-            },
-            {
-              icon: "https://c.animaapp.com/uSmv2gID/img/icon-3.svg",
-              text: "infodesk@srmist.edu.in",
-            },
+            { icon: "https://c.animaapp.com/uSmv2gID/img/icon.svg", text: "+91 44 27417000" },
+            { icon: "https://c.animaapp.com/uSmv2gID/img/icon-1.svg", text: "+91 44 27417777" },
+            { icon: "https://c.animaapp.com/uSmv2gID/img/icon-2.svg", text: "+080 69087000" },
+            { icon: "https://c.animaapp.com/uSmv2gID/img/icon-3.svg", text: "infodesk@srmist.edu.in" },
           ].map((item, index) => (
-            <div key={index} className="flex items-center gap-4">
-              <img src={item.icon} alt="Icon" className="w-[28px] h-[28px]" />
-              <span className="text-lg md:text-xl leading-snug whitespace-nowrap">
-                {item.text}
-              </span>
+            <div key={index} className="flex items-center gap-3">
+              <img src={item.icon} alt="Icon" className="w-[24px] h-[24px]" />
+              <span className="text-base md:text-lg leading-snug">{item.text}</span>
             </div>
           ))}
         </div>
 
         {/* Links Section */}
-        <div className="flex flex-col gap-4 items-start">
+        <div className="flex flex-col gap-4 items-center lg:items-start w-full lg:w-auto text-center lg:text-left">
           {[
-            {
-              icon: "https://c.animaapp.com/uSmv2gID/img/fas-vr-cardboard.svg",
-              text: "Virtual Tour",
-            },
-            {
-              icon: "https://c.animaapp.com/uSmv2gID/img/icon-4.svg",
-              text: "Videos",
-            },
-            {
-              icon: "https://c.animaapp.com/uSmv2gID/img/icon-5.svg",
-              text: "Community Radio",
-            },
+            { icon: "https://c.animaapp.com/uSmv2gID/img/fas-vr-cardboard.svg", text: "Virtual Tour" },
+            { icon: "https://c.animaapp.com/uSmv2gID/img/icon-4.svg", text: "Videos" },
+            { icon: "https://c.animaapp.com/uSmv2gID/img/icon-5.svg", text: "Community Radio" },
           ].map((item, index) => (
-            <div key={index} className="flex items-center gap-4">
+            <div key={index} className="flex items-center gap-3">
               <div
-                className="w-[40px] h-[40px] bg-no-repeat bg-center bg-contain"
+                className="w-[32px] h-[32px] bg-no-repeat bg-center bg-contain"
                 style={{ backgroundImage: `url(${item.icon})` }}
               ></div>
-              <span className="text-lg md:text-xl leading-snug whitespace-nowrap">
-                {item.text}
-              </span>
+              <span className="text-base md:text-lg leading-snug">{item.text}</span>
             </div>
           ))}
         </div>
+
       </div>
     </footer>
   );
